3f1398573e6c24f01baaddd7fe934eff2fff63c2
Auyezkhan Yernar <elenwarface@gmail.com>
2025-11-04 01:35:49 +0500

    removed workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,29 @@ import 'package:workmanager/workmanager.dart';
 import 'screens/welcome_screen.dart';
 import 'services/background_task.dart';
 
-Future<void> requestNotificationPermission() async {
-  final status = await Permission.notification.status;
-  if (!status.isGranted) {
-    await Permission.notification.request();
-  }
-}
+// Future<void> requestNotificationPermission() async {
+//   final status = await Permission.notification.status;
+//   if (!status.isGranted) {
+//     await Permission.notification.request();
+//   }
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await requestNotificationPermission();
+  // await requestNotificationPermission();
   // await Workmanager().initialize(
   //   // callbackDispatcher,
   //   // isInDebugMode: false,
   // );
-  await Workmanager().registerPeriodicTask(
-    'checkWagonStatus',
-    taskName,
-    frequency: const Duration(hours: 2),
-    initialDelay: const Duration(seconds: 5),
-    constraints: Constraints(
-      networkType: NetworkType.connected,
-    ),
-  );
+  // await Workmanager().registerPeriodicTask(
+  //   'checkWagonStatus',
+  //   taskName,
+  //   frequency: const Duration(hours: 2),
+  //   initialDelay: const Duration(seconds: 5),
+  //   constraints: Constraints(
+  //     networkType: NetworkType.connected,
+  //   ),
+  // );
   runApp(ChangeNotifierProvider(
     create: (_) => WagonProvider()..loadWagons(),
     child: const WagonApp(),
